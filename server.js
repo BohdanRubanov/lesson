cdc51412382124c37c191ee3daf16eb1d6e63ed9
Egor1586 <egor115819@gmail.com>
2026-09-09 20:52:17 +0300

    feat/add-endpoint-test

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const PORT = 3000
 app.get('/hello', (req, res)=> {
     res.status(200).json({name: "Sviatoslav"})
 })
+
 app.get('/hello1', (req, res)=> {
     res.status(200).json({name: "Sviatoslav"})
 })
 
+app.get('/test', (req, res)=> {
+    res.status(200).json({name: "Egor"})
+})
 
 app.listen(PORT, HOST, () => {
     console.log(`http://${HOST}:${PORT}`)
