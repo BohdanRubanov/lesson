5f70c4c6b2116ab901114e829bd1112a336ceb75
andrewskulskuia <andrewskulskyia@gmail.com>
2026-09-09 21:26:11 +0300

    sdfsdf

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const HOST = 'localhost'
 const PORT = 3000
 
-app.get('/hello', (req, res)=> {
+app.get('/hellofdsf', (req, res)=> {
     res.status(200).json({name: "Sviatoslav"})
 })
 app.get('/hello1', (req, res)=> {
