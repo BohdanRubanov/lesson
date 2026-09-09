944dc1bcfaacf74b791516488c71d66bab5a8c5b
MaxSubach <subachmax2010@gmail.com>
2026-09-09 20:51:08 +0300

    Create coffee endpoint

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/hello1', (req, res)=> {
     res.status(200).json({name: "Sviatoslav"})
 })
 
+app.get('/coffee', (req, res) => {
+    res.status(418).json({
+    message: "No coffee!"
+    })
+})
 
 app.listen(PORT, HOST, () => {
     console.log(`http://${HOST}:${PORT}`)
